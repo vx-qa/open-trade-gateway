632a834a0f152cc261c42fa296956dd63a454c63
Vincent yu <yutiansut@qq.com>
2018-12-24 18:56:22 +0800

    x

---
+++ config_boost.sh
@@ -0,0 +1,6 @@
+ECHO "install boost 1.68.0" 
+cd ~
+
+wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz && tar -zxvf boost_1_68_0.tar.gz
+
+cd boost_1_68_0 && ./bootstrap.sh && ./b2 && ./b2 install && ldconfig
