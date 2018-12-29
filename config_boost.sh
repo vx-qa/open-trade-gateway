9237db2fa9bd1f1d7d0bc2c9a3974cfcac0fa3b1
Vincent yu <yutiansut@qq.com>
2018-12-30 01:15:04 +0800

    Update config_boost.sh

--- config_boost.sh
+++ config_boost.sh
@@ -1,7 +1,15 @@
 ECHO "install boost 1.68.0" 
+apt update -y && apt install gcc g++  automake autoconf libtool make -y 
+
+
 cd ~
-apt update -y && apt install gcc g++ -y
 
 wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz && tar -zxvf boost_1_68_0.tar.gz
 
 cd boost_1_68_0 && ./bootstrap.sh && ./b2 && ./b2 install && ldconfig
+
+
+
+cd ~
+wget https://www.openssl.org/source/openssl-1.0.2.tar.gz && tar zxvf openssl-1.0.2.tar.gz
+cd ~/openssl-1.0.2 && ./config && make && make install
