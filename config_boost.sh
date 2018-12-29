3b4c89122cc06ebabf1647746119e30ad31109a5
Vincent yu <yutiansut@qq.com>
2018-12-30 00:24:39 +0800

    Update config_boost.sh

--- config_boost.sh
+++ config_boost.sh
@@ -1,5 +1,6 @@
 ECHO "install boost 1.68.0" 
 cd ~
+apt update -y && apt install gcc g++ -y
 
 wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz && tar -zxvf boost_1_68_0.tar.gz
 
